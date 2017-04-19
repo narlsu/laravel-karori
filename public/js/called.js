f218198791b1e96f8d807742438f276d6dff28b6
Zak <wellszak@gmail.com>
2017-04-19 23:16:22 +1200

    more css

--- public/js/called.js
+++ public/js/called.js
@@ -1,6 +1,9 @@
     $(document).ready(function(){
-      $('.carousel.carousel-slider').carousel({fullWidth: true});
+      $('.carousel.carousel-slider').carousel({fullWidth: true}, {duration: 10000});
+      	// window.setInterval(function(){$('.carousel').carousel('next')},4000)
           // Initialize collapse button
+         // Next slide
+		$('.carousel').carousel();
 	  $(".button-collapse").sideNav();
 	  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
 	  //$('.collapsible').collapsible();
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,44 +8,46 @@
 	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
-	@include('partials._nav')
+	<div class="desktop-background">	
+		<div class="desktop-contain">
+			@include('partials._nav')
 
-		
-	
-	<div class="carousel carousel-slider valign-wrapper center" data-indicators="true">
-	    <div class="carousel-fixed-item center">
-	    	<h1 class="header-text valign white-text">Welcome to the Karori Food Co-op</h1>
-	    	<a class="btn waves-effect white grey-text darken-text-2">button</a>
-	    </div>
-	    <div href="#one!">
-	    	<img class="img-responsive carousel-item" src="images/lemonhead.jpg" alt="lemons">
-	    </div>
-	    <div class="carousel-item amber white-text" href="#two!">
-	    </div>
-	    <div class="carousel-item green white-text" href="#three!">
-	    </div>
-	    <div class="carousel-item blue white-text" href="#four!">
-	    </div>
-	  </div>
+				
+			
+			<div class="carousel carousel-slider valign-wrapper center" data-indicators="true">
+			    <div class="carousel-fixed-item center">
+			    	<h1 class="header-text white-text">Welcome to the Karori Food Co-op</h1>
+			    	<a class="btn waves-effect white grey-text darken-text-2">button</a>
+			    </div>
+			    <div class="background-image1 carousel-item c-style" href="#one!">
+			    </div>
+			    <div class="background-image2 carousel-item c-style" href="#two!">
+			    </div>
+			    <div class="background-image3 carousel-item c-style" href="#three!">
+			    </div>
+			    <div class="background-image4 carousel-item c-style" href="#four!">
+			    </div>
+			  </div>
 
 
-	@yield('content')
-	
-</body>
-{{-- Flex Footer --}}
-<footer class="flex-container-foot">
-		<div class="foot-child-cont">
-			<a class="black-anchor footer-item-margin" href="www.facebook.com"><img class="fblogo" src="images/facebooklogo.svg" alt="facebook page link"></a>
-			<p class="hide-on-small-only">Follow us on Facebook!</p>
-		</div>
-		<div class="foot-child-cont">
-			<a class="black-anchor footer-item-margin" href="/contact">
-				<i class="material-icons">email</i>
-			</a>
-			<p class="hide-on-small-only">Any questions? Message Us.</p>
-		</div>
-</footer>
-
+			@yield('content')
+			
+		</body>
+		{{-- Flex Footer --}}
+		<footer class="flex-container-foot">
+				<div class="foot-child-cont">
+					<a class="black-anchor footer-item-margin" href="www.facebook.com"><img class="fblogo" src="images/facebooklogo.svg" alt="facebook page link"></a>
+					<p class="hide-on-small-only">Follow us on Facebook!</p>
+				</div>
+				<div class="foot-child-cont">
+					<a class="black-anchor footer-item-margin" href="/contact">
+						<i class="material-icons">email</i>
+					</a>
+					<p class="hide-on-small-only">Any questions? Message Us.</p>
+				</div>
+		</footer>
+	</div>
+</div>
  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
  <!-- Javascript Linked -->
 <script type="text/javascript" src="/js/materialize.min.js"></script>
