17bf46530daf59b8913e978837cc69cd29b56d9e
zak wells <wellszak@gmail.com>
2017-05-08 16:05:48 +1200

    .

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,9 +48,12 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|max:255',
+            'firstname' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
+            'phonenumber' => 'required|integer|',
+            ''
+
         ]);
     }
 
